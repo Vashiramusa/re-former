c8384bc3e1305f7118bb2508319b0dc250225249
Vashira <vashimgs@gmail.com>
2019-12-26 20:44:06 +0000

    Added forms with form_tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
-    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    @user = User.new(user_params)
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
@@ -16,7 +15,8 @@ class UsersController < ApplicationController
   end
 
   private
-    def user_params
-      params.require(:user).permit(:username, :email, :password)
-    end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
