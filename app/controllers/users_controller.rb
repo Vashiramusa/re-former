726808f59a6a85afc599b78dbc8d4ac33d7066eb
Ermin Cahtarevic <erminc@windowslive.com>
2019-12-26 23:33:32 +0100

    Added username index on users table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
-# frozen_string_literal: true
-
 class UsersController < ApplicationController
   def new
     @user = User.new
   end
 
   def create
+    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
+
     if @user.save
+      redirect_to new_user_path
     else
       render :new
     end
@@ -23,11 +25,10 @@ class UsersController < ApplicationController
     else
       render :edit
     end
-  end
+  end  
 
   private
-
-  def user_params
-    params.require(:user).permit(:username, :email, :password)
-  end
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_26_165734) do
+ActiveRecord::Schema.define(version: 2019_12_26_221924) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_12_26_165734) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
